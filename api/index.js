a6c17bc3a3d75be5098536c21e9caaf3f408ccc6
Paragi Dev <paragidev@gmail.com>
2024-03-05 02:09:57 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MANGO)
+  .then(() => {
+    console.log('📢 Connected to MongoDB ❤️')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(process.env.SERVER_PORT, () => {
+    console.log('🎉 Server is running on port', process.env.SERVER_PORT)
 })
