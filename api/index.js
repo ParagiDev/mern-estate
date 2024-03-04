eb11c1d785d1e9f9f10c9903682ac5bcb7ffd894
Paragi Dev <paragidev@gmail.com>
2024-03-05 05:00:30 +0700

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -15,6 +17,21 @@ mongoose
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(process.env.SERVER_PORT, () => {
     console.log('🎉 Server is running on port', process.env.SERVER_PORT)
 })
+
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    })
+  })
