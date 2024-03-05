2fe208674073d00fac9037b55eddbfa7728289b6
Paragi Dev <paragidev@gmail.com>
2024-03-06 06:41:51 +0700

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,16 +7,19 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
   const hashedPassword = bcryptjs.hashSync(password, 10)
   const newUser = new User({ username, email, password: hashedPassword })
+  
   try {
     await newUser.save()
     res.status(201).json('User created successfully!')
   } catch (error) {
     next(error)
   }
+
 }
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body
+
   try {
     const validUser = await User.findOne({ email })
     if (!validUser) return next(errorHandler(404, 'User not found!'))
@@ -33,4 +36,5 @@ export const signin = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+  
 }
